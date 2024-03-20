ce70e5f441f87e6e16606d7e191e5e42e3f45ba3
fran hernandez <franciscohernandezlopez5@gmail.com>
2024-03-20 20:33:12 +0100

    Add challenge

---
+++ src/app/features/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { AnyCharacter } from '../../../model/interface';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+})
+export class CardComponent {
+  @Input({ required: true }) item!: AnyCharacter;
+}
---
+++ src/model/interface.ts
@@ -0,0 +1,14 @@
+export interface AnyCharacter {
+  name: string;
+  family: string;
+  age: number;
+  type: 'king' | 'fighter' | 'counselor' | 'squire';
+  message: string;
+  isAlive: boolean;
+  kingdomYears?: number;
+  weapon?: string;
+  skillsRange?: number;
+  lord?: object;
+  master?: object;
+  serverRange?: number;
+}
